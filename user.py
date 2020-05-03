60cf598be587a711be32a7d791ca8e3371b8394d
takashi <tk19490612@gmail.com>
2020-05-03 16:55:49 +0900

    年齢追加してみた

--- user.py
+++ user.py
@@ -1,18 +1,18 @@
 class User:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
 
 
 if __name__ == "__main__":
-    user1 = User("Bob")  # Userクラスをインスタンス化
+    user1 = User("Bob", 15)  # Userクラスをインスタンス化
 
     print(user1)  # <__main__.User object at 0x0000016ECE2F7400>
     print(user1.name)  # Bob
 
-    user2 = User("Tom")
+    user2 = User("Tom", 57)
     print(user2)
     print(user2.name)
 
-    user3 = User("Ken")
+    user3 = User("Ken", 49)
     print(user3)
     print(user3.name)
