7a2a70fd0826e348cd74067a90c92d4a6b7593b1
takashi <tk19490612@gmail.com>
2020-05-05 08:48:35 +0900

    クラス / インスタンス / Attribute(属性) / objectを学んだ

--- user.py
+++ user.py
@@ -1,18 +1,26 @@
 class User:
-    def __init__(self, name, age):
+    def __init__(self, name, age, country):
         self.name = name
+        self.age = age
+        self.country = country
 
 
 if __name__ == "__main__":
-    user1 = User("Bob", 15)  # Userクラスをインスタンス化
+    Bob = User("Bob", 15, "USA")  # Userクラスをインスタンス化
 
-    print(user1)  # <__main__.User object at 0x0000016ECE2F7400>
-    print(user1.name)  # Bob
+    print(Bob)  # <__main__.User object at 0x0000016ECE2F7400>
+    print(Bob.name)  # Bob
+    print(Bob.age)  # 15
+    print(Bob.country)  # 15
 
-    user2 = User("Tom", 57)
-    print(user2)
-    print(user2.name)
+    Tom = User("Tom", 57, "USA")
+    print(Tom)
+    print(Tom.name)
+    print(Tom.age)
+    print(Tom.country)
 
-    user3 = User("Ken", 49)
-    print(user3)
-    print(user3.name)
+    Ken = User("Ken", 49, "Japan")
+    print(Ken)
+    print(Ken.name)
+    print(Ken.age)
+    print(Ken.country)
