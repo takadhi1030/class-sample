1917525cd1a64706215a2d7dcf06cca52b7a52bc
takashi <tk19490612@gmail.com>
2020-05-05 08:51:58 +0900

    インスタンス変数と呼ぶらしい

--- user.py
+++ user.py
@@ -8,19 +8,6 @@ class User:
 if __name__ == "__main__":
     Bob = User("Bob", 15, "USA")  # Userクラスをインスタンス化
 
-    print(Bob)  # <__main__.User object at 0x0000016ECE2F7400>
-    print(Bob.name)  # Bob
-    print(Bob.age)  # 15
-    print(Bob.country)  # 15
-
     Tom = User("Tom", 57, "USA")
-    print(Tom)
-    print(Tom.name)
-    print(Tom.age)
-    print(Tom.country)
 
     Ken = User("Ken", 49, "Japan")
-    print(Ken)
-    print(Ken.name)
-    print(Ken.age)
-    print(Ken.country)
