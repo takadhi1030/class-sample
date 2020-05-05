607446c34c9d7237187a840e86df2207e17e4143
takashi <tk19490612@gmail.com>
2020-05-05 09:33:23 +0900

    国籍変更してみた

--- user.py
+++ user.py
@@ -4,10 +4,24 @@ class User:
         self.age = age
         self.country = country
 
+    def display_profil(self):
+        # display_profil() は Userクラスのインスタンスメソッド
+        print(f"{self.name} 国籍: {self.country} {self.age}歳")
+
+    def change_nationality(self, country_name):
+        self.country = country_name
+
 
 if __name__ == "__main__":
     Bob = User("Bob", 15, "USA")  # Userクラスをインスタンス化
-
+    Bob.display_profil()
+    Bob.change_nationality("China")
+    Bob.display_profil()
     Tom = User("Tom", 57, "USA")
-
+    Tom.display_profil()
+    Tom.change_nationality("UK")
+    Tom.display_profil()
     Ken = User("Ken", 49, "Japan")
+    Ken.display_profil()
+    Ken.change_nationality("Korea")
+    Ken.display_profil()
