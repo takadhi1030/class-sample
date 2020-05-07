48c454330de7c7a0a30a8440aba27991f21687f7
takashi <tk19490612@gmail.com>
2020-05-07 20:11:17 +0900

    プログラムの変更なしでできる

--- counter.py
+++ counter.py
@@ -14,31 +14,20 @@
 # print(counter_1.value) 2が出力される
 
 class Counter:
-    def __init__(self, value):
+    def __init__(self, value, step):
         self.value = value
+        self.step = step
 
     def increase(self):
-        self.value += 1
-
-    def increase3(self):
-        self.value += 3
-
+        self.value += self.step
 
 if __name__ == "__main__":
-    counter_1 = Counter(0)
+    counter_1 = Counter(value=0, step=1)
     print(counter_1.value)
-
     counter_1.increase()
     print(counter_1.value)
 
-    counter_1.increase()
-    print(counter_1.value)
-
-    counter_2 = Counter(15)
+    counter_2 = Counter(value=15, step=3)
     print(counter_2.value)
-
-    counter_2.increase3()
-    print(counter_2.value)
-
-    counter_2.increase3()
+    counter_2.increase()
     print(counter_2.value)
